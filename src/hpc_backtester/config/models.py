54195d3dd9afed7c933bce2c589f7ff6630aa454
Harrison <hmj31@pitt.edu>
2026-03-25 15:40:17 -0400

    Initial project setup

---
+++ src/hpc_backtester/config/models.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from typing import Any
+
+@dataclass
+class BacktestConfig:
+    initial_capital: float 
+    commission_pershare: float
+    slippage_bps: float
+    timeframe:str
+    start_date: str
+    end_date: str
+    symbols: list[str]
+
+@dataclass  
+class StrategyConfig:
+    name: str
+    params: dict[str, any]
+
+@dataclass  
+class RuntimeConfig:
+    save_results: bool
+    results_dir: str
+
+@dataclass
+class AppConfig:
+    project_name: str
+    backtest: BacktestConfig
+    Strategy: StrategyConfig
+    runtime: RuntimeConfig
