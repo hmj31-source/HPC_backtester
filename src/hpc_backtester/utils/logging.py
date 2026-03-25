e11e215c2684387d450145d5e6870aa46b936a4d
Harrison <hmj31@pitt.edu>
2026-03-25 17:53:08 -0400

    fixed typo in logging

--- src/hpc_backtester/utils/logging.py
+++ src/hpc_backtester/utils/logging.py
@@ -1,7 +1,7 @@
 import logging
 import sys
 
-def setup_logging(level: int = loggin.INFO) -> None:
+def setup_logging(level: int = logging.INFO) -> None:
     logging.basicConfig(
         level=level,
         format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
